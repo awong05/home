53f9977e3b2f6e8b15f266316c5db889e2790d6a
Andy Wong <Andy.Kee.Wong@gmail.com>
2018-02-25 20:56:50 -0500

    What is love? Baby don't hurt me, don't hurt me, no more.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import path from 'path';
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { StaticRouter as Router } from 'react-router-dom';
 import App from './client/App';
 import Html from './client/Html';
 
@@ -12,7 +12,8 @@ const server = express();
 server.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 server.get('/', (req, res) => {
-  const body = renderToString(<Router><App /></Router>);
+  const context = {};
+  const body = renderToString(<Router context={context} location={req.url}><App /></Router>);
   const title = 'Server side rendering with styled components';
 
   res.send(
